0090922cba56e674f4888b112728b451a64b5edd
ZionChik <supercreative5000@gmail.com>
2024-09-29 00:20:22 +0300

    laggy base movement

--- The Inhuman/Assets/Scripts/Player/Movement.cs
+++ The Inhuman/Assets/Scripts/Player/Movement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.81f;
+
+    private CharacterController characterController;
+    private Vector3 velocity;
+    private Vector3 moveDirection;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        bool isGrounded = characterController.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = Camera.main.transform.right * moveX + Camera.main.transform.forward * moveZ;
+        moveDirection.y = 0;
+        moveDirection.Normalize();
+
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
