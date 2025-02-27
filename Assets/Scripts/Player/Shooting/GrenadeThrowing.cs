a86f326731d188f1a198a127f38e908b63389812
CLANOM2255 <clanom46@gmail.com>
2025-02-27 23:47:37 +0100

    Tasty grenades + bug fixes
    - Fixed line renderer
    - Grenade is working
    - Explosion

--- Assets/Scripts/Player/Shooting/GrenadeThrowing.cs
+++ Assets/Scripts/Player/Shooting/GrenadeThrowing.cs
@@ -8,26 +8,24 @@ public class GrenadeWeapon : WeaponBase
     [SerializeField] private float throwForce = 500f;
     [SerializeField] private float explosionRadius = 5f;
     [SerializeField] private float explosionDamage = 50f;
-    [SerializeField] private float fuseTime = 3f;
+    [SerializeField] private GameObject explosionEffectPrefab;
+    [SerializeField] private float shakeDuration = 0.2f;
+    [SerializeField] private float shakeMagnitude = 0.3f;
+
+    private float fuseTime = 5f;
 
     [SerializeField] private GameObject heldGrenade;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && heldGrenade.activeInHierarchy)
         {
+
             Shoot();
         }
     }
 
-    private void SpawnHeldGrenade()
-    {
-        if (heldGrenade == null)
-        {
-            heldGrenade = Instantiate(heldGrenade, _handPosition.position, _handPosition.rotation, _handPosition);
-        }
-    }
-
-    public override async void Shoot()
+    public override void Shoot()
     { 
         if (heldGrenade == null) Debug.Log("Gooool");
 
@@ -39,34 +37,74 @@ public class GrenadeWeapon : WeaponBase
             rb.AddForce(fpsCam.transform.forward * throwForce, ForceMode.VelocityChange);
         }
 
-        Destroy(heldGrenade);
-        await StartCoroutine(Delay());
-        await Explode(thrownGrenade);
-        await SpawnHeldGrenade();
+        heldGrenade.SetActive(false);
+        StartCoroutine(ThrowingGrenade(thrownGrenade));
+        
     }
 
-    private IEnumerator Delay()
+    private IEnumerator ThrowingGrenade(GameObject grenade)
     {
+        Debug.Log("Throwing Grenade");
+        Debug.Log(fuseTime);
         yield return new WaitForSeconds(fuseTime);
+        Debug.Log("After yield");
+
+        Explode(grenade);
+        
     }
 
     private void Explode(GameObject grenade)
     {
-        Debug.Log("Goida");
+        Debug.Log("Explode");
 
-        Collider[] hitColliders = Physics.OverlapSphere(grenade.transform.position, explosionRadius);
+        StartCoroutine(CameraShake());
 
-        foreach (Collider hit in hitColliders)
+        int enemyLayer = LayerMask.NameToLayer("Enemy");
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        GameObject explosionEffect = Instantiate(explosionEffectPrefab, grenade.transform.position, Quaternion.identity);
+
+        foreach (GameObject obj in allObjects)
         {
-            if (hit.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            if (obj.layer == enemyLayer)
             {
-                if (hit.TryGetComponent(out Enemy enemy))
+                float distance = Vector3.Distance(grenade.transform.position, obj.transform.position);
+                if (distance <= 8f)
                 {
-                    enemy.TakeDamage(explosionDamage);
+                    if (obj.TryGetComponent(out Enemy enemy))
+                    {
+                        enemy.TakeDamage(explosionDamage);
+                    }
                 }
             }
         }
 
         Destroy(grenade);
+        Destroy(explosionEffect, 2f);
+        heldGrenade.SetActive(true);
+    }
+
+    private IEnumerator CameraShake()
+    {
+        Vector3 originalPosition = fpsCam.transform.localPosition;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            fpsCam.transform.localPosition = originalPosition + new Vector3(x, y, 0);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        fpsCam.transform.localPosition = originalPosition;
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("On Destroy");
     }
 }
