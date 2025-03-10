1eb339bceb1d508c9b6acec7dbb7d2e8a0462d7b
ZionChik5000 <supercreative5000@gmail.com>
2025-03-10 18:50:42 +0200

    WIP
    idk

--- Assets/Scripts/Enemy/Enemies/Shooter.cs
+++ Assets/Scripts/Enemy/Enemies/Shooter.cs
@@ -4,6 +4,10 @@ public class Shooter : EnemyMod
 {
     private Enemy enemy;
     [SerializeField] private float retreatDistance = 5f;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float shootDelay = 1f;
+    private float lastShootTime;
 
     public override void Apply(Enemy enemy)
     {
@@ -12,19 +16,40 @@ public class Shooter : EnemyMod
 
     public override void ModUpdate(Enemy enemy)
     {
-        if (!enemy.playerInRadius || enemy.player == null) return;
-
-        if (DistanceToPlayer() < retreatDistance)
-        {
-            Vector3 retreatDirection = (enemy.transform.position - enemy.player.position).normalized;
-            Vector3 targetPosition = enemy.player.position + retreatDirection * retreatDistance;
-            Vector3 moveDirection = (targetPosition - enemy.transform.position).normalized;
-            enemy.agent.Move(moveDirection * enemy.speed * Time.deltaTime);
-        }
+        Shoot();
+        Retret();
     }
 
     private float DistanceToPlayer()
     {
         return Vector3.Distance(enemy.transform.position, enemy.player.position);
+    }
+
+    private void Retret()
+    {
+        if (!enemy.playerInRadius || enemy.player == null) return;
+
+        if (DistanceToPlayer() < retreatDistance)
+        {
+            Vector3 retreatDirection = (enemy.transform.position - enemy.player.position).normalized;
+            retreatDirection.y = 0;
+            Vector3 targetPosition = enemy.player.position + retreatDirection * retreatDistance;
+
+            enemy.agent.SetDestination(targetPosition);
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time - lastShootTime < shootDelay || bulletPrefab == null || shootPoint == null) return;
+
+        if (Physics.Raycast(enemy.transform.position, enemy.transform.forward, out RaycastHit hit))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+                lastShootTime = Time.time;
+            }
+        }
     }
 }
